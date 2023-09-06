928d3305afe86b5528f0111a12b4c0e61e3c6623
LeQP <qle@uoregon.edu>
2023-09-05 17:34:38 -0700

    Added comments to describe how my code works

--- Assets/Scripts/NeedToIncorporate/GravityShift.cs
+++ Assets/Scripts/NeedToIncorporate/GravityShift.cs
@@ -6,115 +6,177 @@ using UnityEngine;
 
 public class GravityShift : MonoBehaviour
 {
+    // The wall that the player will gravity shift to
     Transform targetWall;
-    public static int oritentNum = 0;
     private Rigidbody rb;
+    // Raycasts for checking the player's proximity to any walls behind and in front of them
     private RaycastHit hitFront;
     private RaycastHit hitBack;
+    // Access the PlayerMove script for functions to access each axis's state
     public PlayerMove pm;
+    // Specify which direction that gravity is exerted on the player
     private Vector3 orientVec;
-    private Vector3[] refVecs = {Vector3.down, Vector3.forward, Vector3.left, Vector3.right, Vector3.back,  Vector3.up};
+
+    // An array of vectors that store the different directions that gravity can be exerted on the player
+    private Vector3[] refVecs = { Vector3.down, Vector3.forward, Vector3.left, Vector3.right, Vector3.back, Vector3.up };
     public Transform vCamera;
 
     void Start()
     {
         orientVec = Vector3.down;
         rb = GetComponent<Rigidbody>();
-        pm = GetComponent<PlayerMove>();
     }
 
-    public Vector3 getOrientation() {
-        Vector3 estVec = Vector3.Cross(transform.forward, transform.right * -1);
-        orientVec = new Vector3(Mathf.Round(estVec.x), Mathf.Round(estVec.y), Mathf.Round(estVec.z));
+    // Helper function to specify the current gravity force for other scripts
+    public Vector3 getOrientation()
+    {
         return orientVec;
     }
-    
 
-    bool WallCheck() {
+    // Function to check if a player is close enough to a wall to perform the gravity shift.
+    bool WallCheck()
+    {
         // Approach ground check with using a ray through a RayCast
         float offsetVal = 0.6f;
+        // Create 2 raycasts to check for the wall (front) and the player has enough space to rotate (back) as in not collide with a back wall when gravity shifting
         bool frontCheck = Physics.Raycast(transform.position + (transform.forward * offsetVal), transform.forward * 1f, out hitFront, 2f);
         bool backCheck = Physics.Raycast(transform.position - (transform.forward * offsetVal), transform.forward * -1f, out hitBack, 2f);
-        
-        // hit will contain the distance so check if it corresponds to the object on plane
-	    if(frontCheck && !backCheck) {
+        // Verify that the raycasts was a success and then create a reference to the targeted wall
+        if (frontCheck && !backCheck)
+        {
             targetWall = hitFront.transform;
             return true;
         }
-	    else 
+        else
             return false;
     }
 
-
-    void changeGravity() {
+    // Function responsible for the gravity shift
+    // Uses a locki mechanism so player's can't accidently interfere with the process
+    void changeGravity()
+    {
         pm.keyLock();
-        //Set Vertical Axis
+        // Variables to indicate which axises are going to be used for horizontal or vertical movement
         bool xUsed = false;
         bool yUsed = false;
         bool zUsed = false;
+        /*Set Vertical Axis 
+        1. The currently unused axis now becomes the new vertical axis
 
-        if (orientVec.x != 0) {
-            pm.setAxisState(0, ((int) (orientVec.x * 0.5 + 2.5)));
+        2. This unused axis tends to be the same as the non-zero value of the current gravity direction vector
+
+        3. Use some algebra to properly set the axis state number for the new vertical axis
+        */
+        if (orientVec.x != 0)
+        {
+            pm.setAxisState(0, ((int)(orientVec.x * 0.5 + 2.5)));
             xUsed = true;
         }
-        else if (orientVec.y != 0) {
-            pm.setAxisState(1, ((int) (orientVec.y * 0.5 + 2.5)));
+        else if (orientVec.y != 0)
+        {
+            pm.setAxisState(1, ((int)(orientVec.y * 0.5 + 2.5)));
             yUsed = true;
         }
-        else if (orientVec.z != 0) {
-            pm.setAxisState(2, ((int) (orientVec.z * 0.5 + 2.5)));
+        else if (orientVec.z != 0)
+        {
+            pm.setAxisState(2, ((int)(orientVec.z * 0.5 + 2.5)));
             zUsed = true;
         }
 
-        //Set Horizontal Axis
+        /* 
+        Set Horizontal Axis 
+
+        1. Calculate the distance between the player model's forward vector (where they are facing) to each of the 
+        potential gravity direction vectors (refVecs)
+
+        2. Store those values in an array (compareVecDist). The index corresponds to the index for refVec
+        ex. compareVecDist[0] indicates refVecs[0] or vector3.down's distance from transform.forward
+
+        3. Find the minimum vector out of the array, this indicates the new gravity direction vector because the new gravity direction vector 
+        will  resemble the current player model's forward vector the most. 
+        
+        Ex. player facing right on the main surface with vector3.down as the current vector, so the transform.forward vector
+        will most resemble vector3.right. That means they are facing the right wall and if they perform a gravity shift, then vector3.right will become the new
+        gravity direction. Using the specific compareVecDist value follows this idea when performing the cross 
+        product to determine horizontal movement on the new surface
+        */
+
+        // 1. 
         float[] compareVecDist = new float[6];
         int index = 0;
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 6; i++)
+        {
+            // 2.
             compareVecDist[i] = (refVecs[i] - transform.forward).sqrMagnitude;
         }
-        for (int i = 1; i < 6; i++) {
-            if (compareVecDist[i] < compareVecDist[index]) 
+        // 3.
+        for (int i = 1; i < 6; i++)
+        {
+            if (compareVecDist[i] < compareVecDist[index])
                 index = i;
         }
+
+        // Use cross product between the inverse of the current gravity direction and the new gravity direction
+        // This will output a vector with 3 values, two of which will equal 0 and the third value will equal -1 or 1
+        // And this indicates which axis will be responsible for horizontal movement and whether moving right causes a decrease 
+        // or increase in that axis value. 
         Vector3 rightMov = Vector3.Cross(-1 * orientVec, refVecs[index]);
-        if (rightMov.x != 0) {
-            pm.setAxisState(0, ((int) (rightMov.x * -0.5 + 0.5)));
+
+        // From there, it is a matter of finding which vector axis is the non-zero.
+        // Then algebra is used to calculate the correct state number
+        if (rightMov.x != 0)
+        {
+            pm.setAxisState(0, ((int)(rightMov.x * -0.5 + 0.5)));
             xUsed = true;
         }
-        else if (rightMov.y != 0) {
-            pm.setAxisState(1, ((int) (rightMov.y *  -0.5 + 0.5)));
+        else if (rightMov.y != 0)
+        {
+            pm.setAxisState(1, ((int)(rightMov.y * -0.5 + 0.5)));
             yUsed = true;
         }
-        else if (rightMov.z != 0) {
-            pm.setAxisState(2, ((int) (rightMov.z *  -0.5 + 0.5)));
+        else if (rightMov.z != 0)
+        {
+            pm.setAxisState(2, ((int)(rightMov.z * -0.5 + 0.5)));
             zUsed = true;
         }
-        if (!xUsed) {
+
+        // Respond to the remaining axis that isn't used (enable rotation on this axis and specify its state as unused)
+        if (!xUsed)
+        {
             pm.setAxisState(0, 4);
         }
-            
-        else if (!yUsed) {
+        else if (!yUsed)
+        {
             pm.setAxisState(1, 4);
         }
-            
-        else if (!zUsed) {
+
+        else if (!zUsed)
+        {
             pm.setAxisState(2, 4);
         }
+
+        // Update the current gravity direction
         orientVec = refVecs[index];
         pm.keyUnlock();
+        // Move the player to allow the gravity shift to occur successfully
         pm.pushMov();
     }
 
-    // Update is called once per frame
-    void Update() {
-        float offsetVal = 0.6f;
-        Debug.DrawRay(transform.position + (transform.forward * offsetVal), transform.forward * 2f, Color.red);
-        Debug.DrawRay(transform.position - (transform.forward * offsetVal), transform.forward * -2f, Color.red);
-        if (Input.GetKeyDown(KeyCode.LeftShift)) {
-            if (WallCheck() && !pm.GroundCheck()) {
+
+    // Update responds to the gravity shift command
+    void Update()
+    {
+        //Debug.DrawRay(transform.position + (transform.forward * offsetVal), transform.forward * 2f, Color.red);
+        //Debug.DrawRay(transform.position - (transform.forward * offsetVal), transform.forward * -2f, Color.red);
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            // Verify that the player satisfies their position in regard to walls and that they are grounded
+            if (WallCheck() && !pm.GroundCheck())
+            {
                 changeGravity();
             }
-            else {
+            else
+            {
                 Debug.Log("Can't rotate");
             }
         }
--- Assets/Scripts/NeedToIncorporate/PlayerMove.cs
+++ Assets/Scripts/NeedToIncorporate/PlayerMove.cs
@@ -3,47 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/*
+Terminonology:
+
+Gravity Shift: When the player shifts between walls or surfaces to cause the new wall/surface to be the floor of the player
+Gravity Force: The player uses left-control to alter their mass and amplify the downward gravity force exerted on them
+
+*/
+
+
 public class PlayerMove : MonoBehaviour
 {
 
-    //components
-    public Transform vCamera;
     private Rigidbody rb;
-
-    //constants
+    // 
     public float movSpd = 1f;
     public float groundSpd = 10f;
+    // Default/normal gravity amount to exert on the player
     public float gravity = 5f;
+    // The amount of gravity currently exerted on the player
     private float gravityAmt = 0f;
-    public float airSpd = 2f;
-    public float turnSpd = 1f;
-    public float jumpHeight = 10f;
-    public float jumpTimer = 0.5f;
-    private float timer = 0f;
 
-    //conditions
-    private bool isGrounded;
+    // Base speed value to move the player when in mid air
+    public float airSpd = 2f;
+
+    // Speed value for rotating the player when moving
+    public float turnSpd = 1f;
+    // Jump height for the player when jumping
+    public float jumpHeight = 10f;
+    // Global vector used to store movement values along the x, y, and z axis and use to move the player
+    Vector3 movVec;
+
+    // Vector to indicate direction of where gravity is aimed towards
     private Vector3 orientVec = Vector3.down;
+    // GravityShift script used to access the current gravity vector direction (which also gets altered there during a gravity shift)
     public GravityShift gs;
-    private bool doJump = false;
-    private bool stopJump = false;
-    private bool startTimer = false;
+    // Bools are states used to initate a sequence of code
+    private bool doJump = false; // Activate jump state
+    private bool stopJump = false; // Reverse jump state's effects on player
+    private bool startTimer = false; // Activate timer
+    // Toggle gravity force state
     private bool doForce = false;
-    private bool noForce = true;
+
+    // The amount of time the player is given for each variable jumo
+    public float jumpTimer = 0.5f;
+    // The current time remaining for a variable jump
+    private float timer = 0f;
+    // The altered mass value of the player when using gravity force
+    public float forceMass = 1;
+    // A value to determine how much force should be multiplied when exerting gravity on the player
+    private float forceFact = 1f;
+    // The player's default/normal mass amount
+    private float norMass;
+
+    private bool key = true;
+    public Transform vCamera;
     private bool shiftCamera = false;
 
-
-    //Player Movement Variables
-    private float movHor;
-    private float movVer;
-    Vector3 movVec;
-    public float forceMass = 1;
-    private float forceFact = 1f;
-    private float norMass;
-    private bool key = true;
-
     /*
-    axisState is an array used to decide the player's movement in regards to the world's axis values.
+    axisState[] is an array used to decide the player's movement in regards to the world's axis values.
     Since the player can walk on walls, it changes how the player moves along the 3 axises with the controls.
     To keep track of which axises and direction (+ or -), each index of the array corresponds to the three axises (x, y, and z)
     and have a value 0-4 to indicate how that axis moves.
@@ -53,7 +71,6 @@ public class PlayerMove : MonoBehaviour
     1 = Correspond to the y axis
     2 = Correspond to the z axis
 
-
     Values
     0 = the player's position increases in axis value when the player presses up (Horizontal movement).
     1 = the player's position decreases in axis value when the player presses right (Horizontal movement).
@@ -62,9 +79,22 @@ public class PlayerMove : MonoBehaviour
     4 = the axis is not in used
 
     */
+    private int[] axisState;
+
+    /*
+    axisFreezeRot is an array used to monitor which axises can NOT rotate when on a specific surface.
     
-    private int[] axisState = new int[] { 0, 4, 2 }; //
-    private bool[] axisFreezeRot = new bool[] {true, false, true};
+    Indexes
+    0 = Correspond to the x axis
+    1 = Correspond to the y axis
+    2 = Correspond to the z axis
+
+    Values
+    True = That axis will not rotate
+    False = That axis can rotate
+
+    */
+    private bool[] axisFreezeRot;
 
     // Start is called before the first frame update
     void Start()
@@ -73,73 +103,78 @@ public class PlayerMove : MonoBehaviour
         norMass = rb.mass;
         gravityAmt = gravity;
         timer = jumpTimer;
-
-        // gravity shift is imported class
         orientVec = gs.getOrientation();
-
-        //rotVec = new Vector3(0f, 0.0001f, 0f);
-
+        axisState = new int[3];
+        axisState[0] = 0;
+        axisState[1] = 4;
+        axisState[2] = 2;
+        axisFreezeRot = new bool[3];
+        axisFreezeRot[0] = true;
+        axisFreezeRot[1] = false;
+        axisFreezeRot[2] = true;
     }
 
+    // A small function for the player to move vertically (away from a wall) right after a gravity shift
+    // Also invokes the rotation of the camera
     public void pushMov()
     {
         movVec = orientMove(0f, 0.1f);
         shiftCamera = true;
     }
 
-
-    // On Move Built in Unity function
+    // Unity's input system to allow the player to move in 3D space
     public void OnMove(InputValue input)
     {
+        // Get the left, right, up, and down movement through a 2D vector
         Vector2 inputVec = input.Get<Vector2>();
+
+        // Get how the 2D Vector translates to walking on the current 3D terrain through orientMove()
+        // fixedUpdate() uses the saved movVec to make the player move
         movVec = orientMove(inputVec.x, inputVec.y);
-        Debug.Log(inputVec);
 
     }
 
+    // A function for setting the player's state and ability to rotate on a particular axis
     public void setAxisState(int index, int state)
     {
+        // Set the state for the axis from the input
         axisState[index] = state;
+        // If the axis is not in used, then the player can be rotated on that axis, otherwise the rotation is disabled
         if (state == 4)
-        {
             axisFreezeRot[index] = false;
-        }
         else
-        {
             axisFreezeRot[index] = true;
-        }
     }
 
+    // Lock Mechanism functions
     public void keyLock()
     {
         key = false;
     }
-
     public void keyUnlock()
     {
         key = true;
     }
 
-    public void readAxisState()
+    // Used to determine the axis
+    private float readAxisState(int axisStateNum, float movHor, float movVer)
     {
-        // Debug.Log("X = " + axisState[0]);
-        // Debug.Log("Y = " + axisState[1]);
-        // Debug.Log("Z = " + axisState[2]);
-    }
-
-    private float readAxisState(int axisNum, float movHor, float movVer)
-    {
-        if (axisNum == 0)
+        if (axisStateNum == 0)
             return movHor;
-        else if (axisNum == 1)
+        else if (axisStateNum == 1)
             return -movHor;
-        else if (axisNum == 2)
+        else if (axisStateNum == 2)
             return movVer;
-        else if (axisNum == 3)
+        else if (axisStateNum == 3)
             return -movVer;
-        else
+        else // axisStateNum == 4
             return 0f;
     }
+
+    // A function to output the player's movement in a 3d vector in regards to their current surface and how it
+    // is affected by their orientation.
+    // Each readAxisState() call accesses each axis's state and determines how horizontal and vertical movement are decided
+    // All of the movements are stored into one 3D vector that is returned
     Vector3 orientMove(float movHor, float movVer)
     {
         float x = readAxisState(axisState[0], movHor, movVer);
@@ -148,54 +183,11 @@ public class PlayerMove : MonoBehaviour
         return new Vector3(x, y, z);
     }
 
-    void performJump()
-    {
-        gravityAmt = 0f;
-        rb.AddForce(orientVec * -1 * jumpHeight, ForceMode.Impulse);
-    }
-    void releaseJump()
-    {
-        gravityAmt = gravity;
-    }
-    void jump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            doJump = true;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            stopJump = true;
-        }
-        if (startTimer)
-        {
-            timer -= Time.deltaTime;
-            if (timer <= 0)
-            {
-                stopJump = true;
-            }
-        }
-    }
-    void forceGravity()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            doForce = true;
-        }
-
-        if (Input.GetKeyUp(KeyCode.LeftControl))
-        {
-            doForce = false;
-        }
-    }
-
+    // A function to test whether the player is on the ground or not with a raycast (needs work)
     public bool GroundCheck()
     {
         // Approach ground check with using a ray through a RayCast
         RaycastHit hit;
-        //float height = GetHeight(characterController);
-
         bool groundTest = Physics.Raycast(transform.position + (orientVec * -1), orientVec, out hit, 1.13f);
         if (groundTest)
             return true;
@@ -203,86 +195,116 @@ public class PlayerMove : MonoBehaviour
             return false;
     }
 
+    // The jump has the player's gravity at zero to smoothly add upward force (relative to surface)
+    void performJump()
+    {
+        gravityAmt = 0f;
+        rb.AddForce(orientVec * -1 * jumpHeight, ForceMode.Impulse);
+    }
+    // The jump is done so the gravity goes back to normal to begin dragging the player down
+    void releaseJump()
+    {
+        gravityAmt = gravity;
+    }
+
+    // Allow the player to do variable jumping (height varies based on button held duration)
+    void jump()
+    {
+        // Initiate the jump
+        if (Input.GetKeyDown(KeyCode.Space))
+            doJump = true;
+        // End the jump
+        if (Input.GetKeyUp(KeyCode.Space))
+            stopJump = true;
+        // Use a timer to allow the jump to continue and disable the jump height inecrease when timer is done
+        if (startTimer)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+                stopJump = true;
+        }
+    }
+    // Hold Left Control to allow the player to intensify their gravity (amplfy it) to exert strong downward forces on objects
+    void forceGravity()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+            doForce = true;
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+            doForce = false;
+    }
+
+    // Update() is in charge of responding to player's special actions (jumps and gravity force)
     void Update()
     {
+        // Read jump input
         jump();
+        // Read gravity force input
         forceGravity();
-        if (Input.GetKeyDown(KeyCode.Alpha0))
-        {
-            readAxisState();
-            // Debug.Log("movVec = " + movVec);
-            // Debug.Log("orientVec * -1 = " + orientVec * -1);
-        }
     }
 
-    void Debugger(string quote, Vector3 a, Vector3 b)
-    {
-        // Debug.Log(quote + ": " + Vector3.Cross(a, b));
-    }
-
-    private void processPlanarMovement(){
-        // Determines Player Move speed in air or on ground
-        float modSpd;
-        if (isGrounded){
-            modSpd = groundSpd;
-        }
-        else {
-            modSpd = airSpd;
-        }
-        rb.AddForce(movVec * movSpd * modSpd * rb.mass * forceFact);
-        Debug.Log(movSpd * modSpd * rb.mass * forceFact);
-    }
-
-    private void processPlayerRotation3dPerson(){
-        if (movVec != Vector3.zero){
-            Quaternion newRot = Quaternion.LookRotation(movVec, orientVec * -1);
-            transform.rotation = newRot;
-        }
-    }
-
-
-    // Update is called once per frame
+    // FixedUpdate() It is in charge of operating the game loop of moving the player in accordance to inputs
     void FixedUpdate()
     {
-        isGrounded = GroundCheck();
+        // Key serves as a locking mechanism in events of not interfering with gravity shifting
         if (key)
         {
-            // orientVec = gs.getOrientation();
-            processPlanarMovement();
-            // processPlayerRotation3dPerson();
 
-            // if (shiftCamera)
-            // {
-            //     vCamera.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
-            //     shiftCamera = false;
-            // }
+            // This section alters how the player moves and whether on the ground or in the air
+            // Ideally, the player has less mobility mid-air (similar to real life)
+            bool isGround = GroundCheck();
+            float modSpd = 0f;
+            if (isGround)
+                modSpd = groundSpd;
+            else
+                modSpd = airSpd;
+            rb.AddForce(movVec * movSpd * modSpd * rb.mass * forceFact);
 
+            // Make sure the orientation is up-to-date with the current gravity
+            orientVec = gs.getOrientation();
+            // The player should rotate towards the direction of their movement when moving
+            if (movVec != Vector3.zero)
+            {
+                Quaternion newRot = Quaternion.LookRotation(movVec, orientVec * -1);
+                transform.rotation = newRot;
+            }
 
-            // if (doJump && isGrounded)
-            // {
-            //     performJump();
-            //     startTimer = true;
-            //     doJump = false;
-            // }
-            // if (stopJump)
-            // {
-            //     releaseJump();
-            //     timer = jumpTimer;
-            //     startTimer = false;
-            //     stopJump = false;
-            // }
-            // if (doForce)
-            // {
-            //     forceFact = 1.7f;
-            //     rb.mass = forceMass;
-            //     rb.AddForce(orientVec * gravity * forceMass * 3);
-            //     //doForce = false;
-            // }
-            // if (!doForce)
-            // {
-            //     forceFact = 1f;
-            //     rb.mass = norMass;
-            // }
+            // During a gravity shift, the camera that is following the player needs to rotate to match the player's new orientation (rotation)
+            if (shiftCamera)
+            {
+                //Using .Euler() to match the player's rotation to allow the camera to view them from the back and depict the player in an upright position
+                vCamera.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+                shiftCamera = false;
+            }
+            // The jump signal is detected, so the player performs the initial jump and starts the variable timer
+            if (doJump && isGround)
+            {
+                performJump();
+                startTimer = true;
+                doJump = false;
+            }
+            // The player has let go of the jump button, so the timer is resetted and disabled
+            if (stopJump)
+            {
+                releaseJump();
+                timer = jumpTimer;
+                startTimer = false;
+                stopJump = false;
+            }
+            // The gravity force command is read and the player's mass increases with more gravity force exerted on them
+            if (doForce)
+            {
+                forceFact = 1.7f;
+                rb.mass = forceMass;
+                rb.AddForce(orientVec * gravity * forceMass * 3);
+                //doForce = false;
+            }
+            // The gravity force command is not active so the player's mass and force remain at the default
+            if (!doForce)
+            {
+                forceFact = 1f;
+                rb.mass = norMass;
+            }
+            // Gravity of the current terrain exerts on the player
             rb.AddForce(orientVec * gravityAmt);
         }
     }
