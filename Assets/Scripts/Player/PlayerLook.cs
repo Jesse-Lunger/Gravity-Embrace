a7cd90dcb6410fa9ada7646b2d11073c9841fe00
jesselunger <jlunger@uoregon.edu>
2023-09-07 15:20:30 -0700

    transitioning to third person

--- Assets/Scripts/Player/PlayerLook.cs
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -13,20 +13,20 @@ public class PlayerLook : MonoBehaviour
 
 
     public void ProcessLook(Vector2 input){
-        float mouseX = input.x;
-        float mouseY = input.y;
+        // float mouseX = input.x;
+        // float mouseY = input.y;
 
-        // allows camera to look Vertically
-        xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
+        // // allows camera to look Vertically
+        // xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
 
-        // prevents player from looking past vertical
-        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        // // prevents player from looking past vertical
+        // xRotation = Mathf.Clamp(xRotation, -80f, 80f);
 
-        // rotates just the camera on the y axis, Not entire player
-        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        // // rotates just the camera on the y axis, Not entire player
+        // cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
 
-        // rotates entire player on the x axis.
-        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
+        // // rotates entire player on the x axis.
+        // transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
 
     }
 
